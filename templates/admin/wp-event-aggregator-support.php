023a3b0cd2e03f4430d65697feea457737590a47
Rajat Patel <47205302+Rajat1192@users.noreply.github.com>
2020-01-08 18:45:18 +0530

    Help page plugin list design

--- templates/admin/wp-event-aggregator-support.php
+++ templates/admin/wp-event-aggregator-support.php
@@ -45,52 +45,52 @@ $twitter_url = 'https://twitter.com/XylusThemes/';
             }
         }
         ?>
-        <div class="" style="margin-top: 20px;">
-            <h3 class="setting_bar"><?php _e( 'Plugins you should try','wp-event-aggregator' ); ?></h3>
+        <h3 class="setting_bar"><?php _e( 'Plugins you should try','wp-event-aggregator' ); ?></h3>
+        <div class="plugin-list" style="margin-top: 20px;">
             <?php 
             if( !empty( $plugins ) ){
                 foreach ($plugins as $plugin ) {
-                    ?>
-                    <div class="plugin_box">
-                        <?php if( $plugin->banners['low'] != '' ){ ?>
-                            <img src="<?php echo $plugin->banners['low']; ?>" class="plugin_img" title="<?php echo $plugin->name; ?>">
-                        <?php } ?>                    
-                        <div class="plugin_content">
-                            <h3><?php echo $plugin->name; ?></h3>
-
-                            <?php wp_star_rating( array(
-                            'rating' => $plugin->rating,
-                            'type'   => 'percent',
-                            'number' => $plugin->num_ratings,
-                            ) );?>
-
-                            <?php if( $plugin->version != '' ){ ?>
-                                <p><strong><?php _e( 'Version:','wp-event-aggregator' ); ?> </strong><?php echo $plugin->version; ?></p>
-                            <?php } ?>
-
-                            <?php if( $plugin->requires != '' ){ ?>
-                                <p><strong><?php _e( 'Requires:','wp-event-aggregator' ); ?> </strong> <?php _e( 'WordPress ','wp-event-aggregator' ); echo $plugin->requires; ?>+</p>
-                            <?php } ?>
-
-                            <?php if( $plugin->active_installs != '' ){ ?>
-                                <p><strong><?php _e( 'Active Installs:','wp-event-aggregator' ); ?> </strong><?php echo $plugin->active_installs; ?>+</p>
-                            <?php } ?>
-
-                            <?php //print_r( $plugin ); ?>
-                            <a class="button button-secondary" href="<?php echo admin_url( 'plugin-install.php?tab=plugin-information&plugin='. $plugin->slug.'&TB_iframe=1&width=772&height=600'); ?>" target="_blank">
-                                <?php _e( 'Install Now','wp-event-aggregator' ); ?>
+            ?>
+                <div class="plugin-card">
+                    <div class="plugin-card-top">
+                        <div class="name column-name">
+                            <h3>
+                            <a href="<?php echo admin_url( 'plugin-install.php?tab=plugin-information&plugin='. $plugin->slug.'&TB_iframe=1&width=772&height=600'); ?>" target="_blank" class="thickbox open-plugin-details-modal">
+                            <img src="<?php echo $plugin->banners['high']; ?>" class="plugin-icon" alt="Plugin_photo">
                             </a>
-                            <a class="button button-primary" href="<?php echo $plugin->homepage; ?>" target="_blank">
-                                <?php _e( 'Buy Now','wp-event-aggregator' ); ?>
-                            </a>
+                            </h3>
                         </div>
                     </div>
-                    <?php
+                    <div class="plugin-card-bottom">
+                        <div class="vers column-rating">
+                                <?php wp_star_rating( array(
+                                        'rating' => $plugin->rating,
+                                        'type'   => 'percent',
+                                        'number' => $plugin->num_ratings,
+                                    ) );
+                                ?>
+                        </div>
+                        <div class="column-updated">
+                            <a class="button button-secondary" href="<?php echo admin_url( 'plugin-install.php?tab=plugin-information&plugin=' . $plugin->slug . '&TB_iframe=1&width=772&height=600' ); ?>" target="_blank">
+                            <?php _e( 'Install Now', 'import-eventbrite-events' ); ?>
+                            </a>
+                            <a class="button button-primary" href="<?php echo $plugin->homepage . '?utm_source=crosssell&utm_medium=web&utm_content=supportpage&utm_campaign=freeplugin'; ?>" target="_blank">
+                            <?php _e( 'Buy Now', 'import-eventbrite-events' ); ?>
+                            </a> 			
+                        </div>
+                        <div class="column-downloaded">
+                            <strong><?php echo $plugin->active_installs; ?></strong>+ Active Installations
+                        </div>
+                        <div class="column-compatibility">
+					        <strong>Version:</strong><?php echo $plugin->version;?></span>				
+                        </div>
+                    </div>
+                </div>
+                <?php
                 }
             }
             ?>
             <div style="clear: both;">
         </div>
     </div>
-
 </div>
