2a82cbffa9baf0dcf7efc49853c7834e5bdbecf6
Xylus Dev <support@xylusinfo.com>
2020-06-03 23:56:40 +0530

    Considered private status in check event already exists

--- includes/class-wp-event-aggregator-common.php
+++ includes/class-wp-event-aggregator-common.php
@@ -659,7 +659,7 @@ class WP_Event_Aggregator_Common {
 		$event_id = $centralize_array['ID'];
 		$event_args = array(
 			'post_type' => $post_type,
-			'post_status' => array( 'pending', 'draft', 'publish' ),
+			'post_status' => array( 'pending', 'draft', 'publish', 'private' ),
 			'posts_per_page' => -1,
 			'suppress_filters' => true,
 			'meta_key'   => 'wpea_event_id',
