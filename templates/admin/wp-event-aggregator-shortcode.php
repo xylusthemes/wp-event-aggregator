70b2729c01969a6de17bf5325386e1c70a43a3a7
Rajat1192 <prajat21@gmail.com>
2021-06-07 23:21:19 +0530

    fixed typo

--- templates/admin/wp-event-aggregator-shortcode.php
+++ templates/admin/wp-event-aggregator-shortcode.php
@@ -3,13 +3,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-$ShortcodeTable = new WPEA_Shortcode_List_Table();
-$ShortcodeTable->prepare_items();
+$shortcodeTable = new WPEA_Shortcode_List_Table();
+$shortcodeTable->prepare_items();
 
 ?>
 <div class="wpea_container">
     <div class="wpea_row">
     <h3 class="setting_bar"><?php esc_attr_e( 'WP Event Aggregator Shortcodes', 'wp-event-aggregator' ); ?></h3>
-        <?php $ShortcodeTable->display(); ?>
+        <?php $shortcodeTable->display(); ?>
     </div>
 </div>
