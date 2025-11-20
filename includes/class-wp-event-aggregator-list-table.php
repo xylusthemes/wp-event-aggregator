dcea843ef757ff57ab5152ca409c8e2cfb2749fa
Rajat Patel <prajat21@gmail.com>
2025-11-20 14:11:34 +0530

    update_shortcode_list

--- includes/class-wp-event-aggregator-list-table.php
+++ includes/class-wp-event-aggregator-list-table.php
@@ -873,61 +873,67 @@ class WPEA_Shortcode_List_Table extends WP_List_Table {
                     'action'     	=> '<button class="wpea-btn-copy-shortcode button-primary"  data-value="[wp_events]">Copy</button>',
                     );
 		$data[] = array(
-					'id'            => 2,
+                    'id'            => 2,
+                    'how_to_use'    => 'Ajax Pagination',
+                    'shortcode'     => '<p class="wpea_short_code">[wp_events ajaxpagi="yes" ]</p>',
+                    'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary'  data-value='[wp_events ajaxpagi=\"yes\"]'>Copy</button>",
+                    );
+		$data[] = array(
+					'id'            => 3,
 					'how_to_use'    => 'New Grid Layouts <span style="color:green;font-weight: 900;">( PRO )</span>',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events layout="style2"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary'  data-value='[wp_events layout=\"style2\"]'>Copy</button>",
 			);
 		$data[] = array(
-					'id'            => 3,
+					'id'            => 4,
 					'how_to_use'    => 'New Grid Layouts Style 3',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events layout="style3"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary'  data-value='[wp_events layout=\"style3\"]'>Copy</button>",
 			);
 		$data[] = array(
-					'id'            => 4,
+					'id'            => 5,
 					'how_to_use'    => 'New Grid Layouts Style 4',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events layout="style4"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary'  data-value='[wp_events layout=\"style4\"]'>Copy</button>",
 			);
 		$data[] = array(            
-					'id'            => 5,
+					'id'            => 6,
 					'how_to_use'    => 'Display with column',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events col="2"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events col=\"2\"]' >Copy</button>",
                     );
         $data[] = array(
-                    'id'            => 6,
+                    'id'            => 7,
                     'how_to_use'    => 'Limit for display events',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events posts_per_page="12"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events posts_per_page=\"12\"]' >Copy</button>",
 		);
         $data[] = array(
-                    'id'            => 7,
+                    'id'            => 8,
                     'how_to_use'    => 'Display Events based on order',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events order="asc"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events order=\"asc\"]' >Copy</button>",
                     );
         $data[] = array(
-                    'id'            => 8,
+                    'id'            => 9,
                     'how_to_use'    => 'Display events based on category',
 					'shortcode'     => '<p class="wpea_short_code" >[wp_events category="cat1"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events category=\"cat1\"]' >Copy</button>",
                     );
         $data[] = array(
-                    'id'            => 9,
+                    'id'            => 10,
                     'how_to_use'    => 'Display Past events',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events past_events="yes"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events past_events=\"yes\"]' >Copy</button>",
                     );
         $data[] = array(
-                    'id'            => 10,
+                    'id'            => 11,
                     'how_to_use'    => 'Display Events based on orderby',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events order="asc" orderby="post_title"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events order=\"asc\" orderby=\"post_title\"]' >Copy</button>",
                     );
         $data[] = array(
-                    'id'            => 11,
+                    'id'            => 12,
                     'how_to_use'    => 'Full Short-code',
 					'shortcode'     => '<p class="wpea_short_code">[wp_events  col="2" posts_per_page="12" category="cat1" past_events="yes" order="desc" orderby="post_title" start_date="YYYY-MM-DD" end_date="YYYY-MM-DD"]</p>',
 					'action'     	=> "<button class='wpea-btn-copy-shortcode button-primary' data-value='[wp_events col=\"2\" posts_per_page=\"12\" category=\"cat1\" past_events=\"yes\" order=\"desc\" orderby=\"post_title\" start_date=\"YYYY-MM-DD\" end_date=\"YYYY-MM-DD\"]' >Copy</button>",
