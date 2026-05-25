25eed5479ffbc3a5aca76aaf570ee5eda4c73f2b
Rajat Patel <prajat21@gmail.com>
2026-05-25 16:21:44 +0530

    Switched custom post types from classic editor to Gutenberg

--- wp-event-aggregator.php
+++ wp-event-aggregator.php
@@ -27,7 +27,7 @@ class WP_Event_Aggregator{
 	 * WP_Event_Aggregator The one true WP_Event_Aggregator.
 	 */
 	private static $instance;
-	public $common, $cpt, $eventbrite, $meetup, $facebook, $ical_parser, $ical, $admin, $manage_import, $wpea, $tec, $em, $eventon, $event_organizer, $aioec, $ee4, $my_calendar, $common_pro, $facebook_pro, $eventum, $cron, $fb_authorize, $meetup_authorize, $ical_parser_aioec, $eventprime, $ajax, $eventbrite_api;
+	public $common, $cpt, $eventbrite, $meetup, $facebook, $ical_parser, $ical, $admin, $manage_import, $wpea, $tec, $em, $eventon, $event_organizer, $aioec, $ee4, $my_calendar, $common_pro, $facebook_pro, $eventum, $cron, $fb_authorize, $meetup_authorize, $ical_parser_aioec, $eventprime, $ajax, $eventbrite_api, $htmltoblock;
 
     /**
      * Main WP Event Aggregator Instance.
@@ -56,6 +56,7 @@ class WP_Event_Aggregator{
 
 			self::$instance->includes();
 			self::$instance->common = new WP_Event_Aggregator_Common();
+			self::$instance->htmltoblock = new WP_Event_Aggregator_Html_To_Blocks();
 			self::$instance->ajax   = new WP_Event_Aggregator_Ajax();
 			self::$instance->cpt    = new WP_Event_Aggregator_Cpt();
 			self::$instance->eventbrite = new WP_Event_Aggregator_Eventbrite();
@@ -166,6 +167,7 @@ class WP_Event_Aggregator{
 	private function includes() {
 
 		require_once WPEA_PLUGIN_DIR . 'includes/class-wp-event-aggregator-common.php';
+		require_once WPEA_PLUGIN_DIR . 'includes/class-wp-event-aggregator-html-to-blocks.php';
 		require_once WPEA_PLUGIN_DIR . 'includes/class-wp-event-aggregator-ajax.php';
 		require_once WPEA_PLUGIN_DIR . 'includes/class-wp-event-aggregator-list-table.php';
 		require_once WPEA_PLUGIN_DIR . 'includes/class-wp-event-aggregator-admin.php';
